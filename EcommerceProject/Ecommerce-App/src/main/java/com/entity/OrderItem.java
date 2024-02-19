4b9df7eca5f045648b6d1283dba7a25bfd37c774
asishkumargoudaoupp <156104885+asishkumargoudaoupp@users.noreply.github.com>
2024-02-19 10:01:26 +0530

    Add files via upload
    E-commerce app commit 2

---
+++ EcommerceProject/Ecommerce-App/src/main/java/com/entity/OrderItem.java
@@ -0,0 +1,20 @@
+package com.entity;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderItem {
+	private Map<Product, Integer> items;
+	public OrderItem( Map<Product, Integer> items) {
+		this.items = new HashMap<>(items);
+	}
+	public double calculateTotal() {
+		double total = 0.0;
+		
+		for(Map.Entry<Product, Integer> entry: items.entrySet()) {
+			Product product = entry.getKey();
+			int quantity = entry.getValue();
+			total += product.getPrice()*quantity;
+		}
+		return total;
+	}
+}
